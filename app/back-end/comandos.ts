87addf3ca40af483b9cbc4976ccd8d1666bc261c
hp <hp@pop-os.localdomain>
2023-02-16 17:35:21 -0300

    feat: projeto basico

---
+++ app/back-end/comandos.ts
@@ -0,0 +1,8 @@
+// npm init -y
+// npm install -D typescript@3.3.3 para poder criar o tsconfig.json
+// npm install -D tslint@5.12.1 para poder criar o tsconfig.json
+// npx tsc --init cria o tsconfig.json
+// "build": "npx -y tsc" --> transforma os arquivos .ts em .js
+//  "prestart": "npm run build && npm run db:reset" --> para preencher meu DB
+// "start": "npm run build && node ." --> para rodar o projeto, sem o reload automatico
+//  "dev:tsnd": "tsnd src/server.ts", --> para rodar o projeto, com o reload automatico
---
+++ app/front-end/comandos.js
@@ -0,0 +1,5 @@
+// npm create vite@latest frontend
+// comando para criar o projeto react com vite
+// npm install
+// npm run dev
+// 5173 --> porta que o vite roda
